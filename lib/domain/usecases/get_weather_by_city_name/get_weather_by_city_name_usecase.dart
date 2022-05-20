8cdc7cd328b90878d80c8a4f70bd76388d2a06e0
Daniel Marciano <daniel.castro09@hotmail.com>
2022-05-20 16:30:03 -0300

    ✨ Feature: Created usecase and usecase implementation

--- lib/domain/usecases/get_weather_by_city_name/get_weather_by_city_name_usecase.dart
+++ lib/domain/usecases/get_weather_by_city_name/get_weather_by_city_name_usecase.dart
@@ -1,1 +1,7 @@
-abstract class GetWeatherByCityNameUsecase {} //TODO fazer o usecase
+import 'package:either_dart/either.dart';
+
+import '../../entities/weather_entity.dart';
+
+abstract class GetWeatherByCityNameUsecase {
+  Future<Either<Exception, WeatherEntity>> call(String cityName);
+}
--- lib/domain/usecases/get_weather_by_city_name/get_weather_by_city_name_usecase_impl.dart
+++ lib/domain/usecases/get_weather_by_city_name/get_weather_by_city_name_usecase_impl.dart
@@ -1,4 +1,26 @@
+import 'package:either_dart/either.dart';
+import 'package:weather/domain/contracts/repositories/get_weather_repository.dart';
+import 'package:weather/domain/entities/weather_entity.dart';
 import 'package:weather/domain/usecases/get_weather_by_city_name/get_weather_by_city_name_usecase.dart';
 
 class GetWeatherByCityNameUsecaseImpl implements GetWeatherByCityNameUsecase {
-} //TODO fazer a implementação do usecase
+  final GetWeatherRepository _repository;
+
+  GetWeatherByCityNameUsecaseImpl({required repository})
+      : _repository = repository;
+
+  @override
+  Future<Either<Exception, WeatherEntity>> call(String cityName) async {
+    if (cityName.trim().isEmpty) {
+      return Left(Exception('O nome da cidade não pode estar vazio'));
+    }
+
+    final response = await _repository.call(cityName);
+
+    if (response.isLeft) {
+      return Left(response.left);
+    } else {
+      return Right(response.right);
+    }
+  }
+}
