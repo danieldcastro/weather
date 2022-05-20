6add72ceb91627e4f7490761bb2a2f42c3d29eba
Daniel Marciano <daniel.castro09@hotmail.com>
2022-05-20 18:17:06 -0300

    ✅ Test: Created model tests

--- test/infra/models/remote_weather_model_test.dart
+++ test/infra/models/remote_weather_model_test.dart
@@ -1,1 +1,40 @@
-//TODO fazer testes para o modelo
+import 'package:flutter_test/flutter_test.dart';
+import 'package:weather/infra/models/remote_weather_model.dart';
+
+import '../../mock/weather_mock.dart';
+
+void main() {
+  group('Remote Weather Model', () {
+    test('Should be return a valid RemoteWeatherModel', () {
+      final remoteWeatherModel =
+          RemoteWeatherModel.fromJson(WeatherMock.validWeatherJson);
+
+      expect(remoteWeatherModel, isA<RemoteWeatherModel>());
+      expect(remoteWeatherModel, WeatherMock.validRemoteWeatherModel);
+    });
+
+    test('Should be return a converted entity', () {
+      final convertedEntity =
+          RemoteWeatherModel.fromEntity(WeatherMock.validWeatherEntity);
+
+      expect(convertedEntity, isA<RemoteWeatherModel>());
+      expect(convertedEntity, WeatherMock.validRemoteWeatherModel);
+    });
+
+    test('Should be return toString', () {
+      final remoteWeatherModel =
+          RemoteWeatherModel.fromJson(WeatherMock.validWeatherJson);
+
+      expect(remoteWeatherModel.toString(), isA<String>());
+      expect(remoteWeatherModel.toString(),
+          WeatherMock.validRemoteWeatherModel.toString());
+    });
+
+    test('Should be return hashCode', () {
+      final remoteWeatherModelHashcode =
+          WeatherMock.validRemoteWeatherModel.hashCode;
+
+      expect(remoteWeatherModelHashcode, isA<int>());
+    });
+  });
+}
