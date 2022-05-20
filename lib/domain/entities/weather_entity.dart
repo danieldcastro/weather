a4d48fb960a4844c98cf4db78a88d675b396e9c4
Daniel Marciano <daniel.castro09@hotmail.com>
2022-05-20 16:30:49 -0300

    ✨ Model and entity: Created Weather entity and Remote weather model

--- lib/domain/entities/weather_entity.dart
+++ lib/domain/entities/weather_entity.dart
@@ -1,1 +1,64 @@
-///TODO fazer a entidade
+import 'package:weather/infra/models/remote_weather_model.dart';
+
+class WeatherEntity {
+  int? temp;
+  String? conditionCode;
+  String? description;
+  String? cityName;
+
+  WeatherEntity({
+    this.temp,
+    this.conditionCode,
+    this.description,
+    this.cityName,
+  });
+
+  factory WeatherEntity.fromModel(RemoteWeatherModel model) {
+    return WeatherEntity(
+      temp: model.temp,
+      conditionCode: model.conditionCode,
+      description: model.description,
+      cityName: model.cityName,
+    );
+  }
+
+  factory WeatherEntity.empty() {
+    return WeatherEntity(
+      temp: 0,
+      conditionCode: '',
+      description: '',
+      cityName: '',
+    );
+  }
+
+  @override
+  String toString() {
+    return 'WeatherEntity(temp: $temp, conditionCode: $conditionCode, description: $description, cityName: $cityName)';
+  }
+
+  List<Object?> get props => [
+        temp,
+        conditionCode,
+        description,
+        cityName,
+      ];
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is WeatherEntity &&
+        other.temp == temp &&
+        other.conditionCode == conditionCode &&
+        other.description == description &&
+        other.cityName == cityName;
+  }
+
+  @override
+  int get hashCode {
+    return temp.hashCode ^
+        conditionCode.hashCode ^
+        description.hashCode ^
+        cityName.hashCode;
+  }
+}
--- lib/infra/models/remote_weather_model.dart
+++ lib/infra/models/remote_weather_model.dart
@@ -1,3 +1,74 @@
+import 'dart:convert';
+
 class RemoteWeatherModel {
-  //TODO fazer o modelo
+  int? temp;
+  String? conditionCode;
+  String? description;
+  String? cityName;
+
+  RemoteWeatherModel({
+    this.temp,
+    this.conditionCode,
+    this.description,
+    this.cityName,
+  });
+
+  factory RemoteWeatherModel.fromMap(Map<String, dynamic> data) =>
+      RemoteWeatherModel(
+        temp: data['temp'] as int?,
+        conditionCode: data['condition_code'] as String?,
+        description: data['description'] as String?,
+        cityName: data['city_name'] as String?,
+      );
+
+  Map<String, dynamic> toMap() => {
+        'temp': temp,
+        'condition_code': conditionCode,
+        'description': description,
+        'city_name': cityName,
+      };
+
+  String toJson() => json.encode(toMap());
+
+  factory RemoteWeatherModel.clear() {
+    return RemoteWeatherModel(
+      temp: 0,
+      conditionCode: '',
+      description: '',
+      cityName: '',
+    );
+  }
+
+  factory RemoteWeatherModel.fromEntity(RemoteWeatherModel entity) {
+    return RemoteWeatherModel(
+      temp: entity.temp,
+      conditionCode: entity.conditionCode,
+      description: entity.description,
+      cityName: entity.cityName,
+    );
+  }
+
+  @override
+  String toString() {
+    return 'RemoteWeatherModel(temp: $temp, conditionCode: $conditionCode, description: $description, cityName: $cityName)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is RemoteWeatherModel &&
+        other.temp == temp &&
+        other.conditionCode == conditionCode &&
+        other.description == description &&
+        other.cityName == cityName;
+  }
+
+  @override
+  int get hashCode {
+    return temp.hashCode ^
+        conditionCode.hashCode ^
+        description.hashCode ^
+        cityName.hashCode;
+  }
 }
