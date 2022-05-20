cb6313ed8ba9ad3423550e25c65194d4b3eeae6c
Daniel Marciano <daniel.castro09@hotmail.com>
2022-05-20 18:16:56 -0300

    ✅ Test: Created usecase tests

--- test/domain/usecases/get_weather_by_city_name_usecase_test.dart
+++ test/domain/usecases/get_weather_by_city_name_usecase_test.dart
@@ -1,1 +1,64 @@
-//TODO fazer testes para o usecase
+import 'package:either_dart/either.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:weather/domain/contracts/repositories/get_weather_repository.dart';
+import 'package:weather/domain/entities/weather_entity.dart';
+import 'package:weather/domain/usecases/get_weather_by_city_name/get_weather_by_city_name_usecase.dart';
+import 'package:weather/domain/usecases/get_weather_by_city_name/get_weather_by_city_name_usecase_impl.dart';
+import 'package:weather/infra/errors/failure_request.dart';
+
+import '../../mock/weather_mock.dart';
+
+class GetWeatherRepositoryMock extends Mock implements GetWeatherRepository {}
+
+void main() {
+  late GetWeatherRepository repository;
+  late GetWeatherByCityNameUsecase sut;
+  late WeatherEntity weather;
+
+  setUp(() {
+    repository = GetWeatherRepositoryMock();
+    sut = GetWeatherByCityNameUsecaseImpl(repository: repository);
+    weather = WeatherMock.validWeatherEntity;
+  });
+
+  group('Get weather by city name usecase', () {
+    success() {
+      when(() => repository.call('Jundiai'))
+          .thenAnswer((invocation) async => Right(weather));
+    }
+
+    fail(FailureRequest error) {
+      when(() => repository.call('Jundiai'))
+          .thenAnswer((invocation) async => Left(error));
+    }
+
+    test('Should be return a valid weather', () async {
+      success();
+
+      final result = await sut.call('Jundiai');
+
+      expect(result.right, weather);
+      expect(result.isRight, true);
+      expect(result.right, isA<WeatherEntity>());
+      verify(() => repository.call('Jundiai')).called(1);
+    });
+
+    test('Should be return an error', () async {
+      fail(FailureRequest.badRequest());
+
+      final result = await sut.call('Jundiai');
+
+      expect(result.left, isA<FailureRequest>());
+      expect(result.isLeft, true);
+      expect(result.left, FailureRequest.badRequest());
+    });
+
+    test('Should be return an error when cityName is empty', () async {
+      final result = await sut.call('');
+
+      expect(result.left, isA<Exception>());
+      expect(result.isLeft, true);
+    });
+  });
+}
